a30f2a29e63678a67b5f81712c4858b60e6c2853
vanDerBC <tobiasbc@hiof.no>
2025-04-28 10:55:50 +0200

    oppsett av eksamen
    installert react, satt opp komponenter og css, ryddet i filstruktur, satt opp routing

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import EventPage from './components/EventPage'
+import CategoryPage from './components/CategoryPage'
+import Dashboard from './components/Dashboard'
+
+function App() {
+
+  return (
+  <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="event/:id" element={<EventPage />} />
+      <Route path="category/:slug" element={<CategoryPage />} />
+      <Route path="dashboard" element={<Dashboard />} />
+    </Routes>
+  </>
+  );
+  
+}
+
+export default App
