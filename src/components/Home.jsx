ab9e32fa8b3d4130086871950d1927c45c01d394
Madelenkr <142605372+Madelenkr@users.noreply.github.com>
2025-04-28 14:21:27 +0200

    API
    Skrive ut API i console log

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,23 @@
 export default function Home() {
+
+    let events = ['tons%20of%20rock', 'skeikampenfestivalen', 'Neon', 'Findings'];
+    
+    for (let i = 0; i < events.length; i++) {
+
+    let event = events[i];
+
+    const getData = async () => {
+        fetch(`https://app.ticketmaster.com/discovery/v2/events?apikey=QqvpEAdIbQPJB9GGqnSKAZvmpXwz79Y2&keyword=${event}&locale=*`)
+          .then((response) => response.json())
+          .then((data) => console.log(data))
+          .catch((error) =>
+            console.error("feil under henting fra API", error)
+          );
+      };
+
+    getData();
+    }
+
     return (
         <h1>Home</h1>
     )
