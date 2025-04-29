355cb280b7cb0c71c61fa4ead7270cb744aee115
Madelenkr <142605372+Madelenkr@users.noreply.github.com>
2025-04-29 13:30:28 +0200

    API
    Hentet ut alle 4 id, inn i console

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,11 @@
 export default function Home() {
-
-    let events = ['tons%20of%20rock', 'skeikampenfestivalen', 'Neon', 'Findings'];
     
-    for (let i = 0; i < events.length; i++) {
-
-    let event = events[i];
+   {
 
     const getData = async () => {
-        fetch(`https://app.ticketmaster.com/discovery/v2/events?apikey=QqvpEAdIbQPJB9GGqnSKAZvmpXwz79Y2&keyword=${event}&locale=*`)
+        fetch(`https://app.ticketmaster.com/discovery/v2/attractions?apikey=QqvpEAdIbQPJB9GGqnSKAZvmpXwz79Y2&id=K8vZ917K7fV,%20K8vZ917oWOV,%20K8vZ917_YJf,%20K8vZ917bJC7&locale=*`)
           .then((response) => response.json())
-          .then((data) => console.log(data))
+          .then((data) => console.log(data._embedded.attractions))
           .catch((error) =>
             console.error("feil under henting fra API", error)
           );
