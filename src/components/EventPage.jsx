6807a66369872b4656e6b80c21c6508fe928b1ac
Madelenkr <142605372+Madelenkr@users.noreply.github.com>
2025-04-30 13:24:26 +0200

    EventPage
    Laget en link som er "les mer"

--- src/components/EventPage.jsx
+++ src/components/EventPage.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function EventPage({festival}) {
 
     return (
         <article>
         <h2>{festival.name}</h2>
+        <Link to={festival.id}>Les mer {festival.name}</Link>
       </article>
     );
 }
