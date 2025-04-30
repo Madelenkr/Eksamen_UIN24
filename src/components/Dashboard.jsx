7ae7f18b59dae5173ce5d19ffe5e381ca3940e13
ehsamjo12 <ehsamj@hiof.no>
2025-04-30 14:19:51 +0200

    Lagd en mal for innloging i dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,3 +1,23 @@
 export default function Dashboard() {
-    
-}
+    return (
+      <main>
+        <header>
+          <h1>Logg inn</h1>
+          <p>Fyll ut feltene for å logge inn!</p>
+        </header>
+  
+        <form action="action.page" method="post">
+          <section>
+            <label htmlFor="email">E-post</label>
+            <input type="email"name="email" id="email" placeholder="eksempel@gmail.com"  required/>
+  
+            <label htmlFor="password">Passord</label>
+            <input type="password"name="password" id="password" required/>
+          </section>
+  
+          <button type="submit">Logg inn</button>
+        </form>
+      </main>
+    );
+  }
+  
