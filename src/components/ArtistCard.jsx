0d5390e0f66329b3962815989bafabe736012fcc
Madelenkr <142605372+Madelenkr@users.noreply.github.com>
2025-05-05 11:00:00 +0200

    Ny fetch
    Laget en ny fetch til å finne festivaler

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,17 +1,7 @@
 import "../styles/artistCard.css";
-import Home from "./Home";
 
 export default function ArtistCard({ artist }) {
   return (
-    <article>
-      {attraction?.map((pass) => {
-        const artists = pass._embedded?.attractions;
-        return (
-          <article>
-              <h2>{artists.name}</h2>
-          </article>
-        );
-      })}
-    </article>
+    <h1>Hei</h1>
   );
 }
--- src/components/EventPage.jsx
+++ src/components/EventPage.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"; //Henter parametere fra URLen
 import Layout from "./Layout";
 import "../styles/eventPage.css"; // Importer CSS-modulen
+import ArtistCard from "./ArtistCard";
 
 export default function EventPage() {
 const { id } = useParams(); //Henter id fra URLen
 const [ attraction, setAttraction ] = useState([]); //Statevariabel for å lagre data fra APIen
+const [ sanger, setArtists] = useState([]);
 
   useEffect(() => {
         fetch(`https://app.ticketmaster.com/discovery/v2/attractions/${id}?apikey=QqvpEAdIbQPJB9GGqnSKAZvmpXwz79Y2&id=K8vZ917K7fV,%20K8vZ917oWOV,%20K8vZ917_YJf,%20K8vZ917bJC7&locale=*`) //Hentet de 4 forskjellige API
@@ -16,22 +18,39 @@ const [ attraction, setAttraction ] = useState([]); //Statevariabel for å lagre
           );
       }, [id]); //Legger til id som avhengighet for å oppdatere data når id endres
 
+
+      console.log(attraction)
+    
+      useEffect (() => {
+        fetch(`https://app.ticketmaster.com/discovery/v2/events?apikey=QqvpEAdIbQPJB9GGqnSKAZvmpXwz79Y2&keyword=findings&locale=*`)
+        .then((response) => response.json())
+        .then((data) => setArtists(data._embedded?.attractions))
+        .catch((error) =>
+        console.error("Feil under henting fra API", error)
+      );
+      },[]);
+
   return (
     <>
     <Layout>
     <article>
-      {attraction?.map((pass) => {
+          <section>
+          {sanger?.map((pass) => (
+          <EventCard pass={pass} key={pass.id} />
+          ))};
+            </section>
+      {/*attraction?.map((pass) => {
         const artists = pass._embedded?.attractions;
         return (
           <article>
               <h2>{artists.name}</h2>
           </article>
         );
-      })}
+      })*/}
     </article>
       <h1>{attraction?.name}</h1>
         <p>{attraction?.info}</p>
-        <artistcard attraction={attraction}/>
+        <ArtistCard attraction={attraction}/>
     </Layout>
     </>
   );
