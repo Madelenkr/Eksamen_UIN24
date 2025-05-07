23b1cd895dd7fad89d573b8a5f10300d02190612
SebastianMN12 <sebastian.m.nordstrand@hotmail.no>
2025-05-07 15:09:33 +0200

    liten fiks av css

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -8,7 +8,7 @@ export default function EventCard({festival}) {
     return (
         <article className="event-card">
         {imageUrl && <img src={imageUrl} alt={festival.name} className="festival-image"/>}
-        <h2>{festival.name}</h2>
+        <h2 className="event-card-h2">{festival.name}</h2>
         <Link to={`/event/${festival.id}`} className="event-link">Les mer {festival.name}</Link>
       </article>
     );
