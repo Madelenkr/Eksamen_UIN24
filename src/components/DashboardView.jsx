44d90efe0bdbfc7d1e564982e872dfdced496b35
Madelenkr <142605372+Madelenkr@users.noreply.github.com>
2025-05-13 16:06:04 +0200

    CSS på Dashboard, pluss ryddet litt i dashboardview siden
    CSS i dashboard

--- src/components/DashboardView.jsx
+++ src/components/DashboardView.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
 import "../styles/dashboard.css"; // Importer CSS-modulen
 
 
-export default function DashboardView({ username, onLogout }) {
+export default function DashboardView({ username, onlogout }) {
     return (
         <section className="dashboard">
-            <header className="dashboard-header">
-                <h4 className="dashboard-title">Min side</h4>
-            </header>
+            <article>
+                <h3 className="dashboard-title">Min side</h3>
+            </article>
 
 
-            <nav className="dashboard-nav" aria-label='Min navigasjon'>
+            <nav className="dashboard-nav" aria-label="Min navigasjon">
             <ul className="dashboard-nav-liste">
-                <li className="dashboard-nav-item"><a href="#venner" className="dashboard-nav-link">Venner</a></li>
-                <li className="dashboard-nav-item" ><a href="#kjop" className="dashboard-nav-link">Min kjøp</a></li>
-                <li className="dashboard-nav-item"><a href="#onskeliste" className="dashboard-nav-link">Min ønskeliste</a></li>
+                <li className="dashboard-nav-item"><a href="#">Venner</a></li>
+                <li className="dashboard-nav-item" ><a href="#">Min kjøp</a></li>
+                <li className="dashboard-nav-item"><a href="#">Min ønskeliste</a></li>
             </ul>
             </nav>
         <section className="logout-section">
-            <button onClick={onLogout} className= "logout-button"> Logg ut</button>
+            <button onClick={onlogout} className= "logout-button"> Logg ut</button>
         </section>
         </section>
         
