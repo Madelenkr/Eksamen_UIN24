c41f1e2e22e6cf7c88cee01420739b3fcc179013
Madelenkr <142605372+Madelenkr@users.noreply.github.com>
2025-05-16 12:43:04 +0200

    Laget en ny mappe i static i sanity

---
+++ sanity/static/sanity.config.js
@@ -0,0 +1,13 @@
+export default defineConfig({
+  name: 'default',
+  title: 'Eksamen_UIN2025',
+
+  projectId: 'eqbspp1a',
+  dataset: 'production',
+
+  plugins: [],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
