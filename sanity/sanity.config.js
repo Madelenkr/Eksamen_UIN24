36aa80c8aa1dec3a27b0b49967b387d2ea8ab33b
vanDerBC <tobiasbc@hiof.no>
2025-05-18 16:11:46 +0200

    lagt tilbake kommentarer
    tatt tilbake kommentarer som vi hadde fjernet

--- sanity/sanity.config.js
+++ sanity/sanity.config.js
@@ -4,15 +4,27 @@ import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
+  //Navn på konfigurasjonen
   name: 'default',
+  //Tittel som mappen heter i Sanity
   title: 'Eksamen_UIN2025',
+  //ID til Sanity
 
   projectId: 'eqbspp1a',
+  //Datasett vi bruker
   dataset: 'production',
 
+  //Ekstra verktøy i mappen
+  plugins: [
+    //Verktøyet for å tilpasse meny
+    structureTool(),
+    visionTool()
+  ],
+  //Setter inn skjemaene for dokumenttypen vår
   plugins: [structureTool(), visionTool()],
 
   schema: {
+    //Henter filen fra schematypes
     types: schemaTypes,
   },
 })
--- sanity/schemaTypes/event.js
+++ sanity/schemaTypes/event.js
@@ -1,11 +1,17 @@
+//Eksporterer sanity shcema "event" 
 export const event = {
+  //Navn på feltet
   name: 'event',
+  //Navnet som vises i sanity
   title: 'Event',
+  //Dette er et dokument
   type: 'document',
   fields: [
     {
       name: 'event',
       title: 'Event title',
+      //Data typen tekst
+      type: 'string', 
       type: 'string',
     },
     {
